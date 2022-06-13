b468c9f147d2f998d6c374c76eebaa06d9af7f03
Gamaliel Abanto Quiroz <gamaliel.aq96@gmail.com>
2022-06-13 02:17:06 -0500

    Mapeo entidad usuarios

---
+++ src/test/java/com/app/SpringSecurityRoles/SpringSecurityRolesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.SpringSecurityRoles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityRolesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
